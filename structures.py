4705dbd1f400c4ac3969ed84e8ee953b6b78bdc3
twhughes <tylerwhughes91@gmail.com>
2018-09-21 12:38:02 +0200

    added accelerator example

--- structures.py
+++ structures.py
@@ -2,66 +2,92 @@ import numpy as np
 
 
 def get_grid(shape, dl):
-	# computes the coordinates in the grid
+    # computes the coordinates in the grid
 
-	(Nx,Ny) = shape
+    (Nx, Ny) = shape
 
-	# coordinate vectors
-	x_coord = np.linspace(-Nx/2*dl, Nx/2*dl, Nx)
-	y_coord = np.linspace(-Ny/2*dl, Ny/2*dl, Ny)
+    # coordinate vectors
+    x_coord = np.linspace(-Nx/2*dl, Nx/2*dl, Nx)
+    y_coord = np.linspace(-Ny/2*dl, Ny/2*dl, Ny)
 
-	# x and y coordinate arrays
-	xs, ys = np.meshgrid(x_coord, y_coord, indexing='ij')
-	return (xs, ys)
+    # x and y coordinate arrays
+    xs, ys = np.meshgrid(x_coord, y_coord, indexing='ij')
+    return (xs, ys)
 
 
 def apply_regions(reg_list, xs, ys, eps_start):
-	# constructs the permittivity given a list of regions
+    # constructs the permittivity given a list of regions
 
-	# if it's not a list, make it one
-	if not isinstance(reg_list, list):
-		reg_list = [reg_list]
+    # if it's not a list, make it one
+    if not isinstance(reg_list, list):
+        reg_list = [reg_list]
 
-	# initialize permittivity
-	eps_r = np.ones(xs.shape)
+    # initialize permittivity
+    eps_r = np.ones(xs.shape)
 
-	# loop through lambdas and apply masks
-	for reg in reg_list:
-		reg_vec = np.vectorize(reg)
-		material_mask = reg_vec(xs, ys)
-		eps_r[material_mask] = eps_start
+    # loop through lambdas and apply masks
+    for reg in reg_list:
+        reg_vec = np.vectorize(reg)
+        material_mask = reg_vec(xs, ys)
+        eps_r[material_mask] = eps_start
 
-	return eps_r
+    return eps_r
 
 
 def three_port(L, H, w, d, dl, l, spc, NPML, eps_start):
 
-	# CONSTRUCTS A ONE IN TWO OUT PORT DEVICE
-	# L         : box length in L0
-	# H         : box width  in L0
-	# w         : waveguide widths in L0
-	# d         : distance between out waveguide (centers) in L0
-	# dl        : grid size in L0
-	# shape     : shape of the permittivity output
-	# eps_start : starting relative permittivity
+    # CONSTRUCTS A ONE IN TWO OUT PORT DEVICE
+    # L         : box length in L0
+    # H         : box width  in L0
+    # w         : waveguide widths in L0
+    # d         : distance between out waveguide (centers) in L0
+    # dl        : grid size in L0
+    # shape     : shape of the permittivity output
+    # eps_start : starting relative permittivity
 
-	Nx = 2*NPML[0] + int((2*l + L)/dl)       # num. grids in horizontal
-	Ny = 2*NPML[1] + int((H + 2*spc)/dl)   # num. grids in vertical
-	nx, ny = int(Nx/2), int(Ny/2)            # halfway grid points
-	shape = (Nx,Ny)                          # shape of domain (in num. grids)	
+    Nx = 2*NPML[0] + int((2*l + L)/dl)       # num. grids in horizontal
+    Ny = 2*NPML[1] + int((H + 2*spc)/dl)   # num. grids in vertical
+    nx, ny = int(Nx/2), int(Ny/2)            # halfway grid points
+    shape = (Nx, Ny)                          # shape of domain (in num. grids)  
 
-	# x and y coordinate arrays
-	xs, ys = get_grid(shape, dl)
+    # x and y coordinate arrays
+    xs, ys = get_grid(shape, dl)
 
-	# define regions
-	box    = lambda x, y : (np.abs(x) < L/2) * (np.abs(y) < H/2)
-	wg_in  = lambda x, y : (x < 0)           * (np.abs(y+0.001) < w/2)  # note, this 1e-5 is to fix gridding issues
-	wg_top = lambda x, y : (x > 0)           * (np.abs(y-d/2) < w/2)
-	wg_bot = lambda x, y : (x > 0)           * (np.abs(y+d/2) < w/2)
+    # define regions
+    box    = lambda x, y: (np.abs(x) < L/2) * (np.abs(y) < H/2)
+    wg_in  = lambda x, y: (x < 0)           * (np.abs(y+0.001) < w/2)  # note, this 1e-5 is to fix gridding issues
+    wg_top = lambda x, y: (x > 0)           * (np.abs(y-d/2) < w/2)
+    wg_bot = lambda x, y: (x > 0)           * (np.abs(y+d/2) < w/2)
 
-	reg_list = [box, wg_in, wg_top, wg_bot]
+    reg_list = [box, wg_in, wg_top, wg_bot]
 
-	eps_r = apply_regions(reg_list, xs, ys, eps_start)
+    eps_r = apply_regions(reg_list, xs, ys, eps_start)
 
-	return eps_r
+    return eps_r
 
+
+def accelerator(beta, gap, lambda0, L, spc, dl, NPML, eps_start):
+
+    # CONSTRUCTS A DIELECTRIC LASER ACCELERATOR STRUCTURE
+    # beta      : electron speed / speed of light
+    # gap       : gap size in L0
+    # lambda0   : free space wavelength in L0
+    # L         : length of design region in L0
+    # spc       : distance between PML and src.  src and design region
+    # dl        : grid size in L0
+    # NPML      : number of pml points in x and y
+    # eps_start : starting relative permittivity
+
+    Nx = 2*NPML[0] + int((4*spc + 2*L + gap)/dl)
+    Ny = int(beta*lambda0/dl)
+    nx, ny = int(Nx/2), int(Ny/2)            # halfway grid points
+    shape = (Nx, Ny)                          # shape of domain (in num. grids)
+
+    # x and y coordinate arrays
+    xs, ys = get_grid(shape, dl)
+
+    des = lambda x, y: (np.abs(x) > gap/2) * (np.abs(x) < gap/2 + L)
+
+    eps_r = apply_regions(des, xs, ys, eps_start)
+
+    return eps_r
