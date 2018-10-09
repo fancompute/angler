7a1d658dc2bde3e4e6de2f0d9ec9800a48efe92d
twhughes <tylerwhughes91@gmail.com>
2018-10-09 10:05:44 -0700

    smoothness

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import autograd.numpy as npa
 import matplotlib.pylab as plt
 from functools import wraps
+from autograd.scipy.signal import convolve
 
 """ This is meant to be a place to write convenience functions and objects to
     help with the optimization
@@ -78,46 +79,59 @@ class Binarizer():
             """ Gives a number between 0 and 1 incidating how close each eps_r
                 pixel is to being binarized, used to multiply with J()
             """
+            # material density in design region
+            rho = (eps - 1) / (self.eps_m - 1) * self.design_region
 
-            # eps in design region
-            material_density = (eps - 1) / (self.eps_m - 1)
-
-            import matplotlib.pylab as plt
-
-            density_masked = material_density * self.design_region
-
-            (Nx, Ny) = eps.shape
-            mask_X = np.zeros((Nx, Ny))
-            mask_Y = np.zeros((Nx, Ny))
-
-            for x in range(0, Nx, 2):
-                mask_X[x, :] = 1
-            for y in range(0, Ny, 2):
-                mask_Y[:, y] = 1
-
-            # mask_up = np.zeros(eps.shape)
-            # mask_down = np.zeros(eps.shape)
-            # mask_left = np.zeros(eps.shape)
-            # mask_right = np.zeros(eps.shape)
-
-            # mask_up[1:,:] = self.design_region[:-1,:]
-            # mask_down[:-1,:] = self.design_region[1:,:]
-            # mask_left[:,1:] = self.design_region[:,:-1]
-            # mask_right[:,:-1] = self.design_region[:,1:]
-
-            # number of cells to average over
+            # number of cells in design region
             N = npa.sum(self.design_region)
 
-            f_eps = np.sum(np.abs(mask_X * density_masked)) + np.sum(np.abs(mask_Y * density_masked))
+            # gray level map
+            M_nd = 4 * rho * (1 - rho)
 
-            # average over each cell
-            return f_eps / N
+            # # radius of curvature (pixels)
+            # R = 5
+            (Nx, Ny) = eps.shape
+            # N_conv = min(Nx, Ny) // 4
+            # k = np.zeros((N_conv, N_conv))
+            # xs = np.linspace(-N_conv/2, N_conv/2, N_conv)
+            # ys = np.linspace(-N_conv/2, N_conv/2, N_conv)
+            # for i in range(N_conv):
+            #     for j in range(N_conv):
+            #         k[i, j] = 1*(xs[i]**2 + ys[j]**2 < R**2)
+
+            width = 1
+            N_conv = min(Nx, Ny)
+            k = np.zeros((N_conv))
+            k[N_conv//2:N_conv//2 + width] = 1/width
+
+            N_conv = 2
+            k = np.zeros((N_conv))
+            k[1] = 1
+
+            penalty = 0.0
+            for i in range(Nx):
+                strip_i = M_nd[i,:]
+                output_i = convolve(k, strip_i)
+                # penalty = penalty + 4*npa.sum(output_i*(1-output_i))
+                penalty = penalty + npa.sum(npa.abs(output_i))
+
+            for j in range(Ny):
+                strip_j = M_nd[:,j]
+                output_j = convolve(k, strip_j.T)
+                # penalty = penalty + 4*npa.sum(output_j*(1-output_j))
+                penalty = penalty + npa.sum(npa.abs(output_j))
+
+            # print(penalty, N)
+            penalty = penalty / N / 2
+            return 1 - penalty
 
         # note, this is the actual generator being returned
         # defines how to combine J_bin (binarization function) with original J
         def J_new(*args, **kwargs):
 
             eps = args[2]
+            # print(J_bin(eps))
+
             return J(*args) * J_bin(eps)
 
         return J_new
