71524f34252212b8f8afa617e3a74d05449ffd71
twhughes <tylerwhughes91@gmail.com>
2018-11-14 07:06:05 -0800

    Ny = 1 fix?

--- angler/derivatives.py
+++ angler/derivatives.py
@@ -25,12 +25,15 @@ def createDws(w, s, dL, N, matrix_format=DEFAULT_MATRIX_FORMAT):
             dxb = sp.diags([1, -1, -1], [0, -1, Nx-1], shape=(Nx, Nx))
             Dws = 1/dx*sp.kron(dxb, sp.eye(Ny), format=matrix_format)
     if w is 'y':
-        if s is 'f':
-            dyf = sp.diags([-1, 1, 1], [0, 1, -Ny+1], shape=(Ny, Ny))
-            Dws = 1/dy*sp.kron(sp.eye(Nx), dyf, format=matrix_format)
+        if Ny > 1:
+            if s is 'f':
+                dyf = sp.diags([-1, 1, 1], [0, 1, -Ny+1], shape=(Ny, Ny))
+                Dws = 1/dy*sp.kron(sp.eye(Nx), dyf, format=matrix_format)
+            else:
+                dyb = sp.diags([1, -1, -1], [0, -1, Ny-1], shape=(Ny, Ny))
+                Dws = 1/dy*sp.kron(sp.eye(Nx), dyb, format=matrix_format)
         else:
-            dyb = sp.diags([1, -1, -1], [0, -1, Ny-1], shape=(Ny, Ny))
-            Dws = 1/dy*sp.kron(sp.eye(Nx), dyb, format=matrix_format)
+            Dws = 1
     return Dws
 
 
